40813c5a2b43d3ad3694fbee84a3696840e51e1e
FerdySiregar <ferdysiregar009@gmail.com>
2026-02-23 10:46:34 +0700

    kategori done

--- home.php
+++ home.php
@@ -41,7 +41,7 @@ include 'koneksi.php';
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Total Kategori</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM kategori')); ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM kategori')); ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
@@ -59,7 +59,7 @@ include 'koneksi.php';
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 Total Buku</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM buku')); ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM buku')); ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-book fa-2x text-gray-300"></i>
@@ -79,7 +79,7 @@ include 'koneksi.php';
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM user')); ?></div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM user')); ?></div>
                                                 </div>
                                                 <div class="col">
                                                 </div>
@@ -101,7 +101,7 @@ include 'koneksi.php';
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                                 Total Ulasan</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM ulasan')); ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo mysqli_num_rows(mysqli_query($koneksi, 'SELECT * FROM ulasan')); ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-comments fa-2x text-gray-300"></i>
---
+++ kategori.php
@@ -0,0 +1,62 @@
+<?php
+include 'koneksi.php';
+?>
+<div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Kategori</h1>
+                    <div class="row mb-3">
+                        <a href="?page=kategori_tambah" class="btn btn-primary">Tambah Kategori</a>
+                    </div>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <!-- <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div> -->
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Nama Kategori</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Nama Kategori</th>
+                                            <!-- jika level admin maka tampilkan kolom aksi -->
+                                            <?php if($_SESSION['username']['level'] == 'admin'): ?>
+                                            <th>Aksi</th>
+                                            <?php endif; ?>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php
+                                        $i = 1;
+                                        $query = mysqli_query($koneksi, "SELECT * FROM kategori");
+                                        while ($data = mysqli_fetch_array($query)) :
+                                        ?>
+                                        <tr>
+                                            <td><?=$i++; ?></td>
+                                            <td><?=$data['kategori']; ?></td>
+                                            <td>
+                                                <a href="?page=kategori_ubah&&id=<?= $data['id_kategori']; ?>" 
+                                                    class="btn btn-info">Ubah</a>
+
+                                                <a href="?page=kategori_hapus&&id=<?= $data['id_kategori']; ?>" 
+                                                    onclick="return confirm(apakah anda yakin menhapus data ini?)"
+                                                    class="btn btn-danger">Hapus</a>
+
+                                            </td>
+                                        </tr>
+                                        <?php endwhile; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
